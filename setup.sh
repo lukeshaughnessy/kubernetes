4543ca61a45e565310e65996386c993d842e00a8
luke <luke@lukes-MacBook-Pro.local>
2017-05-11 14:06:03 +0200

    add silent option

--- setup.sh
+++ setup.sh
@@ -52,7 +52,7 @@ EOM
 chmod 600 /root/.ssh/id_rsa
 
 #Install kubectl
-curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+curl -s -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 
 chmod +x ./kubectl
 mv ./kubectl /usr/local/bin/kubectl
