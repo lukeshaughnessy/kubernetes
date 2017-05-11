64dcbd916e9b29a126d781f2728a8c4a03bcf19e
luke <luke@lukes-MacBook-Pro.local>
2017-05-11 13:48:42 +0200

    add kubectl setup

--- setup.sh
+++ setup.sh
@@ -51,6 +51,12 @@ EOM
 
 chmod 600 /root/.ssh/id_rsa
 
+#Install kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+
+chmod +x ./kubectl
+mv ./kubectl /usr/local/bin/kubectl
+
 #Get k8s installer files
 hostname=`hostname`
 if [ $hostname == "launcher" ]
